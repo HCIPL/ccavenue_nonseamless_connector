c613a8ecdb82c2a90242c43d2758bfe77cb521d5
Satyanarayana <satyanarayana.gandham@telehealthindia.com>
2015-11-25 11:44:50 +0530

    simple app with Sinatra without DB and kit provided by CCAvenue

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'sinatra', '~> 1.4.6'
+gem 'sinatra-contrib', '~> 1.4.2'
+gem 'puma',  '~> 2.9.1'
+gem 'activerecord', '~> 4.2.5'
---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'active_record'
+
+require_relative 'crypto'
+
+class CCAvenueNonSeamless < Sinatra::Application
+  register Sinatra::ConfigFile
+
+  config_file './settings.yml'
+  get '/' do
+    erb :index
+  end
+
+  post '/transaction/ccavRequestHandler' do
+    erb :ccavRequestHandler
+  end
+
+  post '/transaction/ccavResponseHandler' do
+    erb :ccavResponseHandler
+  end
+end
