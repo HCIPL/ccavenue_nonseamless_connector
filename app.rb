67857ab59d8c4d20606018ecbeb38d027209802d
Satyanarayana <satyanarayana.gandham@telehealthindia.com>
2015-11-27 13:07:40 +0530

    Removed unnecessary csrf

--- app.rb
+++ app.rb
@@ -1,22 +1,16 @@
 require 'sinatra/base'
 require 'sinatra/config_file'
 require 'active_record'
-require 'rack/csrf'
 require 'securerandom'
 
 require_relative 'crypto'
 
 class CCAvenueNonSeamless < Sinatra::Application
   register Sinatra::ConfigFile
-  use Rack::Csrf, :raise => true, skip: ['POST:/transaction/ccavRequestHandler']
 
   config_file './settings.yml'
-  set :sessions, true
-  # set :session_secret, 'Secret Kye'
-
 
   get '/' do
-    session[:key] = SecureRandom.hex
     erb :index
   end
 
